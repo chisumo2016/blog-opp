f4bca08d761e88e67c82859a05ca47aa68fbff11
=bernard Chisumo <=bchisumo74@yahoo.com>
2018-06-27 12:30:40 +0100

    Displaying the Category Post in the front End

--- category.php
+++ category.php
@@ -1,16 +1,18 @@
+
 <?php
 include "libs/config.php";
 include "libs/Database.php";
 include "function/functions.php";
 
-//Get pecific post
 
+//Get URL id
 $id = $_GET['id'];
 
 //Create an instance
+
 $db = new Database();
 
-$query ="SELECT * FROM posts WHERE id ='$id'";
+$query ="SELECT * FROM posts WHERE category_id='$id'";
 
 $posts = $db->select($query);
 
@@ -61,29 +63,32 @@ $posts = $db->select($query);
 
 <div class="col-sm-8 blog-main">
 
-    <?php $row = $posts->fetch_array()  ?>
-
+    <?php while($row = $posts->fetch_array()) : ?>
         <div class="blog-post">
             <h2 class="blog-post-title"><?php echo $row['title'] ;?></h2>
             <p class="blog-post-meta"><?php echo formatDate($row['date']) ;?> by <a href="#"><?php echo $row['author'] ;?></a></p>
 
-            <img style="float: left; margin-right: 20px; margin-bottom: 10px;" src='images/<?php echo $row['image'] ;?>' width=400" height="300" alt="">
+            <img style="float: left; margin-right: 20px; margin-bottom: 10px;" src='images/<?php echo $row['image'] ;?>' width=200" height="200" alt="">
 
-            <p style="text-align: justify"><?php echo $row['description'] ;?></p>
+            <p style="text-align: justify"><?php echo substr($row['description'] , 0 , 300);?></p>
 
+            <a id="readmore" href="single_post.php?id=<?php echo $row['id'] ;?>">Read More</a>
             <hr>
 
+
         </div><!-- /.blog-post -->
-<!---->
-<!---->
-<!--    <nav class="blog-pagination">-->
-<!--        <a class="btn btn-outline-primary" href="#">Previous</a>-->
-<!--        <a class="btn btn-outline-secondary disabled" href="#">Newer</a>-->
-<!--    </nav>-->
+    <?php endwhile; ?>
+
+    <nav class="blog-pagination">
+        <a class="btn btn-outline-primary" href="#">Previous</a>
+        <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+    </nav>
 
 </div>
 
 <?php
+
 include "includes/sideba.php";
 include "includes/footer.php";
+
 ?>
